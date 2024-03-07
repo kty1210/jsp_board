02306356008e68ca3ec372dca3218b9cd2eb18fe
K\K <kwonabraham@naver.com>
2024-03-08 01:38:33 +0900

    한글깨짐 설정 및 edit 기능 설정 완료

--- src/main/java/board/BoardDAO.java
+++ src/main/java/board/BoardDAO.java
@@ -25,6 +25,8 @@ public class BoardDAO {
 		
 		//동적 SQL문 실행시켜주는 인터페이스
 		PreparedStatement pstmt = null;
+		
+		//쿼리문 '?'는 바인딩 매개변수 아래의 반환값으로 설정
 		String query = "insert into board(num, title, writer, content, regdate, cnt) values(board_seq.nextval, ?, ?, ?, sysdate, 0)";
 		int ret = -1;
 		try {
@@ -165,7 +167,46 @@ public class BoardDAO {
 		}
 		return vo;
 	}
+	
 	//수정(U)
+	public int update(BoardVO vo) {
+		
+		////DB연결 인터페이스
+		Connection con = null;
+		
+		//동적 SQL문 실행시켜주는 인터페이스
+		PreparedStatement pstmt = null;
+		String query = "update board set title=?, content=? where num=?";
+		int ret = -1;
+		try {
+			con = ju.getConnection();
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, vo.getTitle());
+			pstmt.setString(2, vo.getContent());
+			pstmt.setInt(3, vo.getNum());
+			ret = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			//리소스 관리
+			if(pstmt != null) {
+				try {
+					pstmt.close();	
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			if(con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ret;
+	}
+	
 	//삭제(D)
 	
 	
--- src/main/webapp/board/regist.jsp
+++ src/main/webapp/board/regist.jsp
@@ -1,7 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8" import="board.*" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-
+<%
+	request.setCharacterEncoding("utf-8");
+%>
 <jsp:useBean id="vo" class="board.BoardVO"/>
 <jsp:useBean id="dao" class="board.BoardDAO"/>
 <jsp:setProperty name="vo" property="*"/>
